33dee4ef2143f89a7aaabdb9d8bf900aac0f2dd6
LeoMDT <leandro.cahuasqui.m@gmail.com>
2021-08-16 12:22:20 -0500

    Modal

--- app/Views/pais/pais.php
+++ app/Views/pais/pais.php
@@ -6,6 +6,10 @@
                 <p>
                     <a href="<?php echo base_url(); ?>/pais/nuevo" class="btn btn-info">Agregar</a>
                     <a href="<?php echo base_url(); ?>/pais/eliminados" class="btn btn-warning">Eliminados</a>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Launch demo modal
+                    </button>
                 </p>
             </div>
             <div class="card mb-4">
@@ -31,8 +35,8 @@
                                         <a href="<?php echo base_url() . '/pais/editar/' . $dato['id']; ?>" class="btn btn-warning">
                                             <i class="fas fa-pencil-alt"></i>
                                         </a>
-                                        <a href="#" data-href="<?php echo base_url() . '/pais/eliminar/' .
-                                                                    $dato['id']; ?>" data-toggle="modal" data-target="#modal-confirma" data-placement="top" title="Eliminar registro" class="btn btn-danger"><i class="fas fa-trash"></i>
+                                        <a href="#" data-href="<?php echo base_url() . '/pais/eliminar/' . $dato['id']; ?>" data-toggle="modal" data-target="#modal-confirma" data-placement="top" title="Eliminar registro" class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
                                         </a>
                                     </td>
                                 </tr>
@@ -44,6 +48,29 @@
             </div>
         </div>
     </main>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Modal -->
     <div class="modal fade" id="modal-confirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-sm" role="document">
